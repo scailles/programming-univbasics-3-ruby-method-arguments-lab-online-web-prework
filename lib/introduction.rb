1af51ce1a4e133e5bb3041cb94c02c651d5a5e15
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-05 05:35:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,6 @@ def #introduction {name} do
  puts "Hi, my name is #{name}"
 end
 #introduction {Shaina}
+def #introduction_with_language do
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
